68c27316e56e82860856ed54a9a24d625e667f3c
FreightTraindev <mitchell.a.lane@hotmail.com>
2025-09-29 14:41:24 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main() {
      
     //Defining camera
     Camera3D camera = {0};
-    camera.Position = (Vector3){0.0f,5.0f,5.0f};
+    camera.Position = (Vector3){0.0f,10.0f,10.0f};
     camera.target = (Vector3){0.0f,0.0f,0.0f};
     camera.up = (Vector3){0.0f,1.0f,0.0f};
     camera.fovy = 45.0f;
