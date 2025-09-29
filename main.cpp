5d2b90c0ed7c67527ed2b8088a9f234ca66da3e4
Kizzball <kikakifordu@gmail.com>
2025-09-29 12:56:12 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,11 @@ int main() {
     const int screenWidth = 960;
     const int screenHeight = 600;
 
-    InitWindow(screenWidth, screenHeight, "Raylib Starter Template");
+    InitWindow(screenWidth, screenHeight, "Raylib 3d cube");
+     
+    //Defining camera
+    camera3D camera
+    
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
